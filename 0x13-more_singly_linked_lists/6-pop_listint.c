d6ff2a9ee75d55d3a3b96ff64c844ea209fff679
danianaseraldein <danyahnasraldein@gmail.com>
2023-09-26 15:20:26 -0700

    task6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+  * pop_listint - pops off the head of the list and returns its contents
+  * @head:  pointer to the first element in the linked list (head)
+  * Return: data of head
+  */
+int pop_listint(listint_t **head)
+{
+	int i;
+	listint_t *curr, *temp;
+
+	if (head == NULL)
+		return (0);
+	temp = curr = *head;
+	if (*head)
+	{
+		i = curr->n;
+		*head = curr->next;
+		free(temp);
+	}
+	else
+		i = 0;
+	return (i);
+}
