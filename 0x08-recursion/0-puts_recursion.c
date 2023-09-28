896a8f2ddf1425fe2d41b69e19c645f5f2e5c0e3
danianasralein <danyahnasraldein@gmail.com>
2023-09-28 16:29:53 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - Function similar to puts
+ *
+ * @s: parameter for the function
+ */
+void _puts_recursion(char *s)
+{
+	if (s[0] == '\0')
+	{
+		_putchar (10);
+		return;
+	}
+	_putchar(s[0]);
+	s++;
+	_puts_recursion(s);
+}
