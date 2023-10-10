80b4161f0bc09d5ff072f7589ca8e3b210e07da6
danianasralein <danyahnasraldein@gmail.com>
2023-10-10 07:42:55 -0700

    2. File name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *main - prints the name of file 
+ * main - prints the name of file 
  *
- *Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
