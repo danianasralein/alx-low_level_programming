0fc2c77e5bf410fc019cc27039eaf1a9eba1d5c3
dania nasraldein <danyahnasraldein@gmail.com>
2023-10-19 16:09:18 -0700

    1. List length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+  * listint_len -  finds the number of elements in a linked list
+  * @h: singly linked list to print
+  * Return: number of nodes in the list
+  */
+size_t list_len(const list_t *h)
+{
+size_t i;
+
+for (i = 0; h; i++)
+h = h->next;
+return (i);
+}
+
